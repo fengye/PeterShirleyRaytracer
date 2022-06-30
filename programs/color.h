03b07e4eba234b2aff9c60ddbe53468ebd238734
Ye Feng <feng_ye2001@hotmail.com>
2022-06-30 23:02:57 +1200

    Book 1, 8.3. Gamma correction

--- programs/color.h
+++ programs/color.h
@@ -11,10 +11,12 @@ void write_color(std::ostream& out, const color& pixel_color, int samples_per_pi
 	auto g = pixel_color.y();
 	auto b = pixel_color.z();
 
+	// Divide the color by the number of samples and gamma-correct for gamma=2.0.
+	// For gamma 2.2 it should be power(color, 1/2.2)
 	auto one_over_samples = 1.0 / samples_per_pixel;
-	r *= one_over_samples;
-	g *= one_over_samples;
-	b *= one_over_samples;
+	r = sqrt(r * one_over_samples);
+	g = sqrt(g * one_over_samples);
+	b = sqrt(b * one_over_samples);
 
 	out << (int)(255.999 * clamp(r, 0.0, 0.999)) << ' ' 
 		<< (int)(255.999 * clamp(g, 0.0, 0.999)) << ' ' 
