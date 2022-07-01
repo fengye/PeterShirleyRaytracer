4ee84f6a8a5537ed57f9321b840f55772016ea28
Ye Feng <feng_ye2001@hotmail.com>
2022-07-01 14:25:16 +1200

    Book 1, 8.5. More accurate Lambertian

--- programs/vec3.h
+++ programs/vec3.h
@@ -8,6 +8,8 @@
 
 using std::sqrt;
 
+class vec3;
+extern vec3 unit_vector(const vec3& v);
 ////////////////////////////////////////////////////////////////////
 // vec3
 class vec3
@@ -89,6 +91,11 @@ public:
 
 			return v;
 		}
+	}
+
+	static vec3 random_unit_vector()
+	{
+		return unit_vector(random_in_unit_sphere());
 	}
 
 
